1f712507bb63d886b4a526944cf5b97885f46623
mazafaka45 <bx2rootbx2@gmail.com>
2026-07-22 02:26:29 +0200

    Add files via upload

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ echo "[start.sh] launching headless-shell..."
 /headless-shell/headless-shell \
   --remote-debugging-address=127.0.0.1 \
   --remote-debugging-port=9222 \
-  --headless \
+  --headless=new \
   --disable-gpu \
   --no-sandbox \
   --disable-setuid-sandbox \
